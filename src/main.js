9aa872ed0abfcd264349420f8fb39aade0b735c8
Teguh Rianto <teguh.rianto22@gmail.com>
2020-11-16 03:00:12 +0700

    add base styling

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import './assets/scss/theme.scss';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   render: (h) => h(App),
