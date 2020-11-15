d69402922f52a4fa16d97054b8074f8eb21a2e63
Teguh Rianto <teguh.rianto22@gmail.com>
2020-11-16 03:46:47 +0700

    add fontawesome icon & base card weather

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-6">
+          <header class="header" id="header">
+            <nav class="navbar">
+              <div class="navbar-brand">
+                <a href="/">
+                  <img src="./assets/img/logo.png" alt="Simply Weather App" />
+                </a>
+              </div>
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <b-form-checkbox v-model="isDarkMode" name="isDarkMode" switch>
+                    {{ isDarkMode ? 'Dark' : 'Light' }}
+                  </b-form-checkbox>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <b-alert v-model="isCity" variant="info" dismissible>
+            Sorry, City Not Found!
+          </b-alert>
+          <div class="card">
+            <div class="card-header">
+              <form v-on:submit.prevent="getWeather">
+                <input
+                  type="text"
+                  class="form-control shadow-none"
+                  placeholder="Please enter city name..."
+                  v-model="citySearch"
+                  autocomplete="off"
+                />
+              </form>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title mb-0">{{ weather.cityName }}</h5>
+              <h6 class="subtitle">{{ weather.country }}</h6>
+
+              <h1 class="pt-3 mb-0">{{ weather.temperature }}&deg;C</h1>
+              <p>{{ weather.description }}</p>
+
+              <div class="row align-items-center justify-content-between">
+                <div class="col">
+                  <h6>{{ weather.lowTemperature }}&deg;C</h6>
+                </div>
+                <div class="col">
+                  <h6>{{ weather.highTemperature }}&deg;C</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      citySearch: '',
+      isCity: false,
+      isDarkMode: false,
+      isVisible: false,
+      weather: {
+        cityName: 'Bandung',
+        country: 'ID',
+        temperature: 22,
+        description: 'Clouds everywhere',
+        lowTemperature: '19',
+        highTemperature: '30',
+        feelsLike: '20',
+        humidity: '55',
+      },
+    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import './assets/scss/theme.scss';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+library.add(fas, far, fab);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   render: (h) => h(App),
