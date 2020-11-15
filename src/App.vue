f7c5f0a53b2543a8619138efb3c28f4b8d295734
Teguh Rianto <teguh.rianto22@gmail.com>
2020-11-16 04:35:55 +0700

    fix search position

--- src/App.vue
+++ src/App.vue
@@ -19,63 +19,61 @@
               </ul>
             </nav>
           </header>
-          <div class="card mb-4">
-            <div class="card-header">
-              <form v-on:submit.prevent="getWeather">
-                <input
-                  type="text"
-                  class="form-control shadow-none"
-                  placeholder="Please enter city name..."
-                  v-model="citySearch"
-                  autocomplete="off"
-                />
-              </form>
-              <b-alert v-model="isCity" variant="info" dismissible class="mt-3 mb-0">
-                City not found! Please try another city
-              </b-alert>
+          <form v-on:submit.prevent="getWeather">
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control shadow-none text-center"
+                placeholder="Please enter city name..."
+                v-model="citySearch"
+                autocomplete="off"
+              />
             </div>
-            <div v-if="isVisible">
-              <div class="card-body text-center">
-                <h5 class="card-title mb-0 text-capitalize">
-                  {{ weather.cityName }} (<span class="text-uppercase">{{ weather.country }}</span>)
-                </h5>
+          </form>
+          <b-alert v-model="isCity" variant="info" dismissible class="mt-3 mb-0">
+            City not found! Please try another city
+          </b-alert>
+          <div class="card mb-4" v-if="isVisible">
+            <div class="card-body text-center">
+              <h5 class="card-title mb-0 text-capitalize">
+                {{ weather.cityName }} (<span class="text-uppercase">{{ weather.country }}</span>)
+              </h5>
 
-                <img :src="weather.icon" alt="Weather Icon">
+              <img :src="weather.icon" alt="Weather Icon">
 
-                <h1 class="mb-0">{{ weather.temperature }}&deg;C</h1>
-                <p class="text-capitalize">{{ weather.description }}</p>
+              <h1 class="mb-0">{{ weather.temperature }}&deg;C</h1>
+              <p class="text-capitalize">{{ weather.description }}</p>
 
-                <div class="row align-items-center justify-content-between">
-                  <div class="col">
-                    <h5 class="mb-0">
-                      <font-awesome-icon
-                        :icon="['fas', 'long-arrow-alt-down']"
-                        class="mr-1 text-primary"></font-awesome-icon>
-                      {{ weather.lowTemperature }}&deg;C
-                    </h5>
-                    <small>Low Temperature</small>
-                  </div>
-                  <div class="col">
-                    <h5 class="mb-0">
-                      <font-awesome-icon
-                        :icon="['fas', 'long-arrow-alt-up']"
-                        class="mr-1 text-danger"></font-awesome-icon>
-                      {{ weather.highTemperature }}&deg;C
-                    </h5>
-                    <small>High temperature</small>
-                  </div>
+              <div class="row align-items-center justify-content-between">
+                <div class="col">
+                  <h5 class="mb-0">
+                    <font-awesome-icon
+                      :icon="['fas', 'long-arrow-alt-down']"
+                      class="mr-1 text-primary"></font-awesome-icon>
+                    {{ weather.lowTemperature }}&deg;C
+                  </h5>
+                  <small>Low Temperature</small>
+                </div>
+                <div class="col">
+                  <h5 class="mb-0">
+                    <font-awesome-icon
+                      :icon="['fas', 'long-arrow-alt-up']"
+                      class="mr-1 text-danger"></font-awesome-icon>
+                    {{ weather.highTemperature }}&deg;C
+                  </h5>
+                  <small>High temperature</small>
                 </div>
               </div>
-              <div class="card-footer text-center">
-                <div class="row align-items-center justify-content-between">
-                  <div class="col">
-                    <p class="mb-0">{{ weather.feelsLike }}&deg;C</p>
-                    <small>Feels like</small>
-                  </div>
-                  <div class="col">
-                    <p class="mb-0">{{ weather.humidity }}%</p>
-                    <small>Humidity</small>
-                  </div>
+            </div>
+            <div class="card-footer text-center">
+              <div class="row align-items-center justify-content-between">
+                <div class="col">
+                  <p class="mb-0">{{ weather.feelsLike }}&deg;C</p>
+                  <small>Feels like</small>
+                </div>
+                <div class="col">
+                  <p class="mb-0">{{ weather.humidity }}%</p>
+                  <small>Humidity</small>
                 </div>
               </div>
             </div>
